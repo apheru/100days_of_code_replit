2c045bb5dbf22b50bf31449a103c80d760630319
Apheru <pythonic.apheru@gmail.com>
2024-10-23 03:19:50 -0300

    day 02

---
+++ day002/main.py
@@ -0,0 +1,14 @@
+# Versão em inglês da atividade
+
+my_name = input("What's your name? ")
+my_age = input("How old are you? ")
+print("Gee, that's REALLY OLD")
+replit = input("Do you like Replit? ")
+print("OF COURSE YOU DO!")
+print()
+print("So, you are")
+print(my_name)
+print("and are the ripe old age of")
+print(my_age)
+print("and clearly think that Replit is")
+print(replit)
---
+++ day002/main_pt.py
@@ -0,0 +1,14 @@
+# Versão em português
+
+my_name = input("Qual o seu nome? ")
+my_age = input("Quantos anos você tem? ")
+print("Putz! Cê tá VELHO MESMO!")
+replit = input("Você gosta do Replit? ")
+print("Claro que você gosta!")
+print()
+print("Então.. você é")
+print(my_name)
+print("e está na idade avançada de")
+print(f"{my_age} anos de idade")
+print("e claramente pensa que o Replit é...")
+print(replit)

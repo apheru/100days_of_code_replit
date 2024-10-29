b5d79e666cf5253f73b268098947edd4e8c12d4a
Apheru <pythonic.apheru@gmail.com>
2024-10-29 01:24:21 -0300

    correção de  bugs desafio dia 07

--- day007/challenger.py
+++ day007/challenger.py
@@ -7,7 +7,7 @@ if like_one_piece == "sim":
     favorite_mugiwara = input("Qual seu mugiwara favorito? ")
     if favorite_mugiwara == "luffy":
         job_ship_luffy = input("Escolheu bem! Luffy é um líder destemido que sonha em ser o Rei dos Piratas! Sabe qual é o papel dele no bando? ")
-        if job_ship_luffy == "capitão" or "capitao":
+        if job_ship_luffy == "capitão" or job_ship_luffy == "capitao":
             print("Acertou! Luffy é o capitão do bando, liderando todos com coragem! 👒")
         else:
             print("Quase! Mas esse não é o papel de Luffy! 🥴")
@@ -27,37 +27,37 @@ if like_one_piece == "sim":
         job_ship_usopp = input("Usopp, o 'corajoso' e criativo! Ele sempre traz algo inesperado! Qual será o papel dele na tripulação? ")
         if job_ship_usopp == "atirador":
             print("Usopp, o corajoso e criativo! Ele sempre traz algo inesperado! Qual será o papel dele na tripulação? 🔫")
-        elif job_ship_usopp == "capitão" or "capitao":
+        elif job_ship_usopp == "capitão" or job_ship_usopp == "capitao":
             print("Ha ha ha, então você acredita nas histórias do próprio Usopp, hein? Ele até se acha o 'Capitão Usopp' de vez em quando! Mas, na realidade, o nosso bravo guerreiro do mar ainda não ocupa esse cargo! 😅")
         else:
             print("Não foi dessa vez. Tente de novo com outro papel para Usopp. 😮")
     elif favorite_mugiwara == "nami":
-        job_ship_nami = input("Nami, sempre brilhante e estrategista! Uma ótima escolha! Você sabe qual função ela exerce no bando? 🍊")
+        job_ship_nami = input("Nami, sempre brilhante e estrategista! Uma ótima escolha! Você sabe qual função ela exerce no bando? ")
         if job_ship_nami == "navegadora":
-            print("Acertou em cheio! Nami é a navegadora do bando! 🗺")
+            print("Acertou em cheio! Nami é a navegadora do bando! 🗺🍊")
         else:
             print("Errou... essa função não é da Nami. 😪")
     elif favorite_mugiwara == "chopper":
-        job_ship_chopper = input("Chopper! A adorável rena que sempre ajuda a todos no bando! Consegue dizer qual é o papel dele no badno? Além de roubar nossos corações com tamanha fofura! 😍😍")
-        if job_ship_chopper == "médico" or "medico":
+        job_ship_chopper = input("Chopper! A adorável rena que sempre ajuda a todos no bando! Consegue dizer qual é o papel dele no bando? Além de roubar nossos corações com tamanha fofura! 😍😍 ")
+        if job_ship_chopper == "médico" or job_ship_chopper == "medico":
             print("Isso mesmo! Além de ser super fofo, o Chopper é o médico do grupo! 🩺")
         else:
             print("Não exatamente... esse não é o papel do nosso pequenino Chopper. 😫")
-    elif favorite_mugiwara == "nico robin" or "robin":
+    elif favorite_mugiwara == "nico robin" or favorite_mugiwara == "robin":
         job_ship_robin = input("Robin, a sábia e enigmática! Ela traz muito conhecimento ao grupo. Você sabe qual é a função dela na tripulação? 📚📚")
-        if job_ship_robin == "arqueóloga" or "arqueologa":
+        if job_ship_robin == "arqueóloga" or job_ship_robin == "arqueologa":
             print("Correto! Robin é a arqueóloga da tripulação. 🏛")
         else:
             print("Não foi dessa vez. Robin tem outra função no bando. 🤗")
     elif favorite_mugiwara == "franky":
         job_ship_franky = input("Franky, sempre trazendo estilo e invenções únicas para o bando! Mas qual é a função dele no grupo? 🥤")
-        if job_ship_franky == "engenheiro" or "mecanico":
+        if job_ship_franky == "engenheiro" or job_ship_franky == "mecanico" or job_ship_franky == "mecânico":
             print("Acertou! Franky é o engenheiro da tripulação! ⚙")
         else:
             print("Não é isso... Franky desempenha outro papel. 😶")
     elif favorite_mugiwara == "brook":
         job_ship_brook = input("Brook, o animado e inusitado! Ele sempre traz alegria ao grupo! Sabe qual é o papel dele na tripulação? 💀")
-        if job_ship_brook == "músico" or "musico":
+        if job_ship_brook == "músico" or job_ship_brook == "musico":
             print("Perfeito! Brook é o músico do bando! 🎻")
         else:
             print("Quase, mas essa não é a função de Brook. 💀")
